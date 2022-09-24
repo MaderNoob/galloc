f93ae45dffaa1c6c1dac59c382a1a882b789061e
Roee Shoshani <roysho10@gmail.com>
2022-09-24 18:12:28 +0300

    implemented allocating without start padding

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,22 @@ const MIN_ALIGNMENT: usize = USIZE_ALIGNMENT;
 const MIN_FREE_CHUNK_SIZE: usize = core::mem::size_of::<FreeChunk>() + USIZE_SIZE;
 const HEADER_SIZE: usize = core::mem::size_of::<Chunk>();
 
+/// Chunk size must be divible by 4, even if `USIZE_ALIGNMENT` is smaller than 4,
+/// but if `USIZE_ALIGNMENT` is bigger than 4 then size must be a multiple of it.
+const CHUNK_SIZE_ALIGNMENT: usize = core::cmp::max(USIZE_ALIGNMENT, 4);
+
 pub struct Allocator {
     heap_region: HeapRegion,
     free_chunk: Option<FreeChunkPtr>,
 }
 impl Allocator {
     fn alloc(&mut self, layout: core::alloc::Layout) -> *mut u8 {
-        let layout_size = unsafe { align_up(core::cmp::max(layout.size(), 2 * USIZE_SIZE), 4) };
+        let layout_size = unsafe {
+            align_up(
+                core::cmp::max(layout.size(), 2 * USIZE_SIZE),
+                CHUNK_SIZE_ALIGNMENT,
+            )
+        };
         let layout_align = core::cmp::max(layout.align(), MIN_ALIGNMENT);
 
         let cur_chunk_ref_from_back = &mut self.free_chunk;
@@ -25,7 +34,7 @@ impl Allocator {
                 // already aligned
                 if let Some(ptr) =
                     self.heap_region
-                        .alloc_aligned(layout_size, layout_align, cur_chunk)
+                        .alloc_aligned(layout_size, cur_chunk, cur_chunk_ref_from_back)
                 {
                     return ptr.as_ptr();
                 }
@@ -44,8 +53,8 @@ impl Allocator {
 }
 
 pub struct HeapRegion {
-    heap_start: usize,
-    heap_end: usize,
+    heap_start_addr: usize,
+    heap_end_addr: usize,
 }
 
 impl HeapRegion {
@@ -88,10 +97,95 @@ impl HeapRegion {
     fn alloc_aligned(
         &mut self,
         layout_size: usize,
-        layout_align: usize,
         cur_chunk: FreeChunkRef,
+        cur_chunk_ref_from_back: &mut Option<FreeChunkPtr>,
     ) -> Option<NonNull<u8>> {
-        todo!()
+        let cur_chunk_size = cur_chunk.header.size();
+        // first make sure that the chunk is big enough to hold the allocation.
+        if cur_chunk_size < layout_size {
+            return None;
+        }
+
+        // check if we need any end padding
+        let end_padding = cur_chunk_size - layout_size;
+        if end_padding > 0 {
+            // check if the end padding is large enough to hold a free chunk
+            if end_padding >= MIN_FREE_CHUNK_SIZE {
+                // if the end padding is large enough to hold a free chunk, create a chunk there.
+                Some(unsafe {
+                    // this is safe because we checked that `end_padding` is big enough
+                    self.alloc_split_end_padding_chunk(
+                        layout_size,
+                        end_padding,
+                        cur_chunk,
+                        cur_chunk_ref_from_back,
+                    )
+                })
+            } else {
+                // if the end padding is not large enough to hold a free chunk, consider it a part
+                // of the allocated chunk. this is a little wasteful, but it prevents us from
+                // returning `null` from the allocator even when we have enough space.
+
+                // this case can be considered the same as allocating without any end padding.
+                Some(self.alloc_no_end_padding(cur_chunk, cur_chunk_ref_from_back))
+            }
+        } else {
+            // if there is no end padding
+            Some(self.alloc_no_end_padding(cur_chunk, cur_chunk_ref_from_back))
+        }
+    }
+
+    fn alloc_no_end_padding(
+        &mut self,
+        cur_chunk: FreeChunkRef,
+        cur_chunk_ref_from_back: &mut Option<FreeChunkPtr>,
+    ) -> NonNull<u8> {
+        // mark the chunk as used and make the necessary updates
+        cur_chunk.mark_as_used_unlink(cur_chunk_ref_from_back, self.heap_end_addr);
+
+        // retrun the chunk to the user
+        unsafe { NonNull::new_unchecked(cur_chunk as *mut _ as *mut u8) }
+    }
+
+    /// Allocates the given chunk and splits an end padding chunk from it.
+    ///
+    /// # Safety
+    ///
+    /// `end_padding` must be greater than or equal to [`MIN_FREE_CHUNK_SIZE`].
+    unsafe fn alloc_split_end_padding_chunk(
+        &mut self,
+        layout_size: usize,
+        end_padding: usize,
+        cur_chunk: FreeChunkRef,
+        cur_chunk_ref_from_back: &mut Option<FreeChunkPtr>,
+    ) -> NonNull<u8> {
+        // the end address of the allocated chunk that will be returned to the user.
+        // this is also the start address of the end padding chunk.
+        let allocated_chunk_end_addr = cur_chunk.header.content_addr() + layout_size;
+
+        // this unsafe block splits an end padding chunk from the current chunk and marks it
+        // as used. it returns a reference to `cur_chunk` but as a `UsedChunkRef`.
+        let allocated_chunk = unsafe {
+            // this new chunk will be put inside the linked list of free chunks in place
+            // of the current chunk, so its `fd` should be the `fd` of the current chunk.
+            let end_padding_chunk =
+                FreeChunk::create_new(allocated_chunk_end_addr, end_padding, cur_chunk.fd);
+
+            // there is no need to update the next chunk because it's prev in use bit is
+            // already set to false, as it should be.
+
+            // we must now make sure that some free chunks points to the created end padding
+            // chunk, to make sure that it's part of the linked list.
+            // we can do that by marking the current chunk as used, and placing the end padding
+            // chunk in place of it.
+            cur_chunk.mark_as_used(
+                cur_chunk_ref_from_back,
+                Some(end_padding_chunk),
+                self.heap_end_addr,
+            )
+        };
+
+        NonNull::new_unchecked(allocated_chunk as *mut _ as *mut u8)
     }
 }
 
@@ -104,8 +198,7 @@ pub struct UsedChunk(Chunk);
 #[repr(C)]
 pub struct FreeChunk {
     header: Chunk,
-    fd: Option<FreeChunkRef>,
-    // bk: Option<FreeChunkRef>,
+    fd: Option<FreeChunkPtr>,
 }
 
 pub type FreeChunkRef = &'static mut FreeChunk;
@@ -130,6 +223,20 @@ impl Chunk {
         } else {
             ChunkRef::Used(core::mem::transmute(chunk))
         }
+    }
+
+    /// Creates a new chunk header, if the size is divisible by 4.
+    fn new(size: usize, is_free: bool, prev_in_use: bool) -> Option<Self> {
+        DivisbleBy4Usize::new(size, is_free, prev_in_use).map(Self)
+    }
+
+    /// Creates a new chunk header.
+    ///
+    /// # Safety
+    ///
+    /// `size` must be divisible by 4.
+    unsafe fn new_unchecked(size: usize, is_free: bool, prev_in_use: bool) -> Self {
+        Self(DivisbleBy4Usize::new_unchecked(size, is_free, prev_in_use))
     }
 
     /// The size of the chunk.
@@ -163,8 +270,8 @@ impl Chunk {
     }
 
     /// Sets whether the previous chunk is considered free or not.
-    fn set_prev_in_use(&mut self, is_prev_chunk_free: bool) {
-        self.0.set_additional_bit2(is_prev_chunk_free)
+    fn set_prev_in_use(&mut self, prev_in_use: bool) {
+        self.0.set_additional_bit2(prev_in_use)
     }
 
     /// The address where this chunk starts.
@@ -226,6 +333,73 @@ impl FreeChunk {
         self.header.set_size(new_size);
         let postfix_size_ptr = (self.header.end_addr() - USIZE_SIZE) as *mut usize;
         unsafe { *postfix_size_ptr = new_size }
+    }
+
+    /// Marks this free chunk as used, updates its next chunk, and replaces
+    /// the current chunk in the linked list of free chunks with
+    /// `replace_in_linked_list_with`.
+    pub fn mark_as_used(
+        &mut self,
+        cur_chunk_ref_from_back: &mut Option<FreeChunkPtr>,
+        replace_in_linked_list_with: Option<FreeChunkPtr>,
+        heap_end_addr: usize,
+    ) -> UsedChunkRef {
+        // mark as used
+        self.header.set_is_free(false);
+
+        // update next chunk, if there is one
+        if let Some(next_chunk_addr) = self.header.next_chunk_addr(heap_end_addr) {
+            let next_chunk = unsafe { UsedChunk::from_addr(next_chunk_addr) };
+            next_chunk.0.set_prev_in_use(true);
+        }
+
+        // unlink the current chunk from the linked list
+        *cur_chunk_ref_from_back = replace_in_linked_list_with;
+
+        unsafe { core::mem::transmute(self) }
+    }
+
+    /// Marks this free chunk as used, updates its next chunk, and unlinks this chunk
+    /// from the linked list of free chunks.
+    pub fn mark_as_used_unlink(
+        &mut self,
+        cur_chunk_ref_from_back: &mut Option<FreeChunkPtr>,
+        heap_end_addr: usize,
+    ) {
+        self.mark_as_used(cur_chunk_ref_from_back, self.fd, heap_end_addr)
+    }
+
+    /// Creates a new free chunk at the given address, with the given size.
+    /// The new chunk will point to `fd`, will be marked as free, and its `prev_in_use`
+    /// flag will be set to `true`, because no 2 free chunks can be adjacent.
+    ///
+    /// # Safety
+    ///
+    ///  - `addr` must be a valid non-null memory address which is not used by any other chunk.
+    ///  - `size` must be divisible by 4.
+    ///  - The chunk must be pointed at from a back chunk that is in the linked list of free chunks.
+    ///  - The chunk's next chunk, if any, must be updated that its previous chunk is now free.
+    pub unsafe fn create_new(addr: usize, size: usize, fd: Option<FreeChunkPtr>) -> FreeChunkPtr {
+        let ptr = addr as *mut FreeChunk;
+
+        // write the chunk header and content
+        *ptr = FreeChunk {
+            // last argument is the `prev_in_use` flag, and there are no 2 adjacent free
+            // chunks, so the previous chunk is surely unused, thus last argument is `false`.
+            header: Chunk::new_unchecked(size, true, false),
+            fd,
+        };
+
+        // dont forget to write the postfix size at the end of the chunk
+        //
+        // first find the end address of this chunk
+        let end_addr = addr + HEADER_SIZE + size;
+        // use the end address to find the address of the posfix size
+        let postfix_size_ptr = (end_addr - USIZE_SIZE) as *mut usize;
+        // write the posfix size
+        unsafe { *postfix_size_ptr = size }
+
+        FreeChunkPtr::new_unchecked(ptr)
     }
 }
 
